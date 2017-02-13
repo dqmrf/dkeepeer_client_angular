42d360fc8c5ef677e98adc4ca9c83680418de286
misha-pelykh <jscript@meta.ua>
2017-02-13 16:08:25 +0200

    added express js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+const port = process.env.PORT || '4200';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log(`API running on localhost:${port}`));
