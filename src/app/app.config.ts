e7dfa0aa5d9fb5d303d4832f95e23c967c8b476a
misha-pelykh <jscript@meta.ua>
2017-02-13 10:09:51 +0200

    change config and node version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,9 +10,9 @@ export class AppConfig {
 }
 
 const CONFIG_PRODUCTION: AppConfig = {
-  serverUrl: "http://localhost:3000",
-  apiEndpoint: "http://localhost:3000/api",
-  clientId: "051476c1165ee4094f9102db43c5a70381f1dc6cce8012aa855dbc935d126541"
+  serverUrl: "https://dkeeper-api-server.herokuapp.com",
+  apiEndpoint: "https://dkeeper-api-server.herokuapp.com/api",
+  clientId: "b6633ae982516d3f54a80749bc5c6b3749e3cededed1cf8b52bbd7386bec8b86"
 };
 
 const CONFIG_DEVELOPMENT: AppConfig = {
